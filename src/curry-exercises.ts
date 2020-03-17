4ba4dad9036500546aa0d549f16a016229c246d4
zhangaz1 <zhangaz1@hotmail.com>
2020-03-17 14:41:52 +0800

    complete exercises-1

--- src/curry-exercises.ts
+++ src/curry-exercises.ts
@@ -1,4 +1,4 @@
-import R from 'ramda';
+import R, { comparator } from 'ramda';
 
 
 // 练习 1
@@ -7,28 +7,35 @@ import R from 'ramda';
 
 export const words = R.split(/\s/ig);
 
-// // 练习 1a
-// //==============
-// // 使用 `map` 创建一个新的 `words` 函数，使之能够操作字符串数组
+// 练习 1a
+//==============
+// 使用 `map` 创建一个新的 `words` 函数，使之能够操作字符串数组
 
-// var sentences = undefined;
-
-
-// // 练习 2
-// //==============
-// // 通过局部调用（partial apply）移除所有参数
-
-// var filterQs = function(xs) {
-//   return filter(function(x){ return match(/q/i, x);  }, xs);
-// };
+export const sentences = R.map(words);
 
 
-// // 练习 3
-// //==============
-// // 使用帮助函数 `_keepHighest` 重构 `max` 使之成为 curry 函数
+// 练习 2
+//==============
+// 通过局部调用（partial apply）移除所有参数
 
-// // 无须改动:
-// var _keepHighest = function(x,y){ return x >= y ? x : y; };
+// export const filterQs = function (xs: string[]) {
+// 	return R.filter(function (x) { return R.not(R.isEmpty(R.match(/q/i, x))); }, xs);
+// }
+
+const matchQ = R.match(/q/ig);
+const hasQ = R.compose(R.not, R.isEmpty, matchQ);
+export const filterQs = R.filter(hasQ);
+
+
+// 练习 3
+//==============
+// 使用帮助函数 `_keepHighest` 重构 `max` 使之成为 curry 函数
+
+// 无须改动:
+// type compareEnable = number | string;
+// const _keepHighest = <A>(x: A, y: A): A => x >= y ? x : y;
+const _keepHighest = (x: number, y: number): number => x >= y ? x : y;
+export const max = R.reduce(_keepHighest, -Infinity);
 
 // // 重构这段代码:
 // var max = function(xs) {
@@ -38,14 +45,16 @@ export const words = R.split(/\s/ig);
 // };
 
 
-// // 彩蛋 1:
-// // ============
-// // 包裹数组的 `slice` 函数使之成为 curry 函数
-// // //[1,2,3].slice(0, 2)
-// var slice = undefined;
+// 彩蛋 1:
+// ============
+// 包裹数组的 `slice` 函数使之成为 curry 函数
+// //[1,2,3].slice(0, 2)
+export const slice = R.curry((start: number, end: number, xs: any[]) => {
+	return xs.slice(start, end);
+});
 
 
-// // 彩蛋 2:
-// // ============
-// // 借助 `slice` 定义一个 `take` curry 函数，该函数调用后可以取出字符串的前 n 个字符。
-// var take = undefined;
+// 彩蛋 2:
+// ============
+// 借助 `slice` 定义一个 `take` curry 函数，该函数调用后可以取出字符串的前 n 个字符。
+export const take = slice(0);
--- test/curry-exercises.test.ts
+++ test/curry-exercises.test.ts
@@ -1,8 +1,59 @@
-// import { } from 'jest';
+import {
+	words,
+	sentences,
+	filterQs,
+	max,
+	slice,
+	take,
+} from '../src/curry-exercises';
 
-import { words } from '../src/curry-exercises';
+const expectEqual = (expected: any, actual: any) => expect(actual).toEqual(expected);
+const expectBe = (expected: any, actual: any) => expect(actual).toBe(expected);
 
-test('words', () => {
-	expect(words('Jingle bells Batman smells'))
-		.toEqual(['Jingle', 'bells', 'Batman', 'smells']);
+describe('curry-exercises', () => {
+	test('words', () => {
+		const result = words('Jingle bells Batman smells');
+		const expected = ['Jingle', 'bells', 'Batman', 'smells'];
+		expectEqual(result, expected);
+	});
+
+	test('sentences', () => {
+		const result =
+			sentences([
+				"Jingle bells Batman smells",
+				"Robin laid an egg"
+			])
+			;
+		const expected = [
+			['Jingle', 'bells', 'Batman', 'smells'],
+			['Robin', 'laid', 'an', 'egg']
+		];
+		expectEqual(result, expected);
+	});
+
+	test('filterQs', () => {
+		const result =
+			filterQs(['quick', 'camels', 'quarry', 'over', 'quails'])
+			;
+		const expected = ['quick', 'quarry', 'quails'];
+		expectEqual(result, expected);
+	});
+
+	test('max', () => {
+		const result = max([323, 523, 554, 123, 5234, 88]);
+		const expected = 5234;
+		expectBe(result, expected);
+	});
+
+	test('slice', () => {
+		const result = slice(2, 5, [1, 2, 3, 4, 5, 6, 7]);
+		const expected = [3, 4, 5];
+		expectEqual(result, expected);
+	});
+
+	test('take', () => {
+		const result = take(2, ['a', 'b', 'c']);
+		const expected = ['a', 'b'];
+		expectEqual(result, expected);
+	});
 });
